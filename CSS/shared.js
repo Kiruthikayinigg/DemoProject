e416d2ca6b2fefcd19373387f5cc13ed2dabde80
kiruthikayini <skiruthikayini@greatergoods.com>
2022-06-07 18:13:30 +0530

    changes commit

--- CSS/shared.js
+++ CSS/shared.js
@@ -13,8 +13,11 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
     // modal.style.display = "block";
     // backdrop.style.display = "block";
     // modal.className = 'open'; // This will actually overwrite the complete class list
-    modal.classList.add(".open");
-    backdrop.classList.add(".open");
+    modal.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(function() {
+      backdrop.classList.add("open");
+    }, 10);
   });
 }
 
@@ -35,11 +38,17 @@ function closeModal() {
     modal.classList.remove("open");
   }
   backdrop.classList.remove("open");
+  setTimeout(function() {
+    backdrop.style.display = "none";
+  }, 200);
 }
 
 toggleButton.addEventListener("click", function() {
   // mobileNav.style.display = 'block';
   // backdrop.style.display = 'block';
   mobileNav.classList.add("open");
-  backdrop.classList.add("open");
+  backdrop.style.display = "block";
+  setTimeout(function() {
+    backdrop.classList.add("open");
+  }, 10);
 });
